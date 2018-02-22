e745b77ccfe8308c36e395b6e2e065d82b07ac91
Joe <joe.l.filbrun@gmail.com>
2018-02-21 19:31:19 -0500

    added init.lua

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # ER301-Revolver
 A round robin polyphonic sample player unit for the Orthogonal Devices ER-301 Sound Computer
 
-2018-02-21 First commit
---
+++ init.lua
@@ -0,0 +1,12 @@
+local units = {
+  {category="Oscillators"},
+  {title="Revolver",moduleName="Revolver",keywords="source"},
+}
+
+return {
+  title = "First Unit",
+  name = "revolver",
+  contact = "joe.l.filbrun@gmail.com",
+  keyword = "test",
+  units = units
+}
